1bbe730e474d4614a88413ae336663bd5ad7e0bb
jungho3145 <jungho3145@gmail.com>
2020-07-15 20:45:30 +0900

    Revert "lastCommit"
    This reverts commit a4542594749ed9a0d8e75f6ee6f6adb0655e6489.

--- CSP/MainForm.cs
+++ CSP/MainForm.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,8 +15,7 @@ namespace CSP
 {
     public partial class MainForm : Form
     {
-        public int type = 1;
-        public int selectedOredrId;//1 - order 2 - student 3 - notebook
+        public int type = 1; //1 - order 2 - student 3 - notebook
         public MainForm()
         {
             InitializeComponent();
@@ -25,31 +24,42 @@ namespace CSP
         private void btnCheck_Click(object sender, EventArgs e)
         {
             string result = textBox1.Text;
-            DataSet ds = new DataSet();
-            using (SqlConnection conn = new SqlConnection(new dataBase().path))
+            try
             {
-                conn.Open();
-                string sql = "SELECT * FROM ordernotebook WHERE stuid=" + textBox1.Text;
-                SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
-                adapter.Fill(ds, "order");
+                DataSet ds = new DataSet();
+                using (SqlConnection conn = new SqlConnection(new dataBase().path))
+                {
+                    conn.Open();
+                    string sql = "SELECT orderid FROM order WHERE stuid=" + textBox1.Text;
+                    SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+                    adapter.Fill(ds, "order");
+                }
+                dataGridView1.DataSource = ds.Tables[0];
+            } catch
+            {
+                MessageBox.Show("문제가 발생했습니다.\n입력을 확인해주세요");
             }
-            dataGridView1.DataSource = ds.Tables[0];
-            type = 1;
 
         }
 
         private void btnAll_Click(object sender, EventArgs e)
         {
-            DataSet ds = new DataSet();
-            using (SqlConnection conn = new SqlConnection(new dataBase().path))
+            try
             {
-                conn.Open();
-                string sql = "SELECT * FROM ordernotebook";
-                SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
-                adapter.Fill(ds, "order");
+                DataSet ds = new DataSet();
+                using (SqlConnection conn = new SqlConnection(new dataBase().path))
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM order";
+                    SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+                    adapter.Fill(ds, "order");
+                }
+                dataGridView1.DataSource = ds.Tables[0];
+                type = 1;
+            } catch
+            {
+                MessageBox.Show("DB에 테이블이 정확하게 있는지 확인해주세요");
             }
-            dataGridView1.DataSource = ds.Tables[0];
-            
         }
 
         private void btnTime_Click(object sender, EventArgs e)
@@ -70,7 +80,7 @@ namespace CSP
                 MessageBox.Show("제대로 입력한게 맞으신가요?\n확인이 필요합니다.");
                 return;
             }
-
+/*
             result = Interaction.InputBox("조회할 주를 입력하세요", "학생 노트북 대여 관리 시스템", "1~4");
             int refus = 1;
             try // 숫자를 입력한게 맞는지 확인
@@ -87,54 +97,59 @@ namespace CSP
             {
                 MessageBox.Show("제대로 입력한게 맞으신가요?\n확인이 필요합니다.");
                 return;
-            }
-            string search_dt = "";
-            if (month < 10)
+            }*/
+            string search_dt = month + ".%";
+
+            try
             {
-                search_dt = "0" + month + "/" + refus;
-            }
-            else
+                DataSet ds = new DataSet();
+                using (SqlConnection conn = new SqlConnection(new dataBase().path))
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM order WHERE date LIKE '" + search_dt + "';";
+                    SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+                    adapter.Fill(ds, "order");
+                }
+                dataGridView1.DataSource = ds.Tables[0];
+                type = 1;
+            } catch
             {
-                search_dt = month + "/" + refus;
+                MessageBox.Show("문제가 발생했습니다.\nDB를 확인해주세요.");
             }
-            DataSet ds = new DataSet();
-            using (SqlConnection conn = new SqlConnection(new dataBase().path))
-            {
-                conn.Open();
-                string sql = "SELECT * FROM ordernotebook WHERE date='" + search_dt + "';";
-                SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
-                adapter.Fill(ds, "order");
-            }
-            dataGridView1.DataSource = ds.Tables[0];
-            type = 1;
         }
 
         private void btnName_Click(object sender, EventArgs e)
         {
             string result = Interaction.InputBox("조회할 학생의 이름을 입력해주세요", "학생 노트북 대여 관리 시스템", "홍길동");
 
-            DataSet ds = new DataSet();
-            using (SqlConnection conn = new SqlConnection(new dataBase().path))
+            try
             {
-                conn.Open();
-                string sql = "SELECT * FROM student, ordernotebook WHERE ordernotebook.stuid = student.stuid AND stuname = '" + result + "';";
-                SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
-                adapter.Fill(ds, "ordernotebook");
+                DataSet ds = new DataSet();
+                using (SqlConnection conn = new SqlConnection(new dataBase().path))
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM order WHERE name=" + result + ";";
+                    SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+                    adapter.Fill(ds, "order");
+                }
+                dataGridView1.DataSource = ds.Tables[0];
+                type = 1;
+            } catch
+            {
+                MessageBox.Show("문제가 발생했습니다.\n조회에 실패했습니다.");
             }
-            dataGridView1.DataSource = ds.Tables[0];
-            type = 1;
+            
         }
 
         private void btn_add_Click(object sender, EventArgs e)
         {
-       
+            int num = 10;
             string result = inputName.Text;
             double dt = 1.0;
-            string date = DateTime.Now.ToString("MM") + "/" + GetWeekOfMonth(DateTime.Now);
+            string date = DateTime.Now.ToString("MM.dd");
             try // 숫자를 입력한게 맞는지 확인
             {
-                dt = double.Parse(result.Split('-')[0]);
-                dt = double.Parse(result.Split('-')[1]);
+                dt = double.Parse(result);
             }
             catch
             {
@@ -150,7 +165,7 @@ namespace CSP
                 SqlCommand command = new SqlCommand();
 
                 command.Connection = conn;
-                command.CommandText = "INSERT INTO ordernotebook(date, stuid, notebookid) VALUES('" + date + "','" + stid + "','" + nbid + "');";
+                command.CommandText = "INSERT INTO order(orderid, date, stuid, notebookid) VALUES('" + num + "','" + date + "','" + stid + "','" + nbid + "');";
                 command.ExecuteNonQuery();
 
                 btnAll_Click(null, null);
@@ -160,13 +175,13 @@ namespace CSP
 
         private void btn_del_Click(object sender, EventArgs e)
         {
-        
+            string orderid = textBox1.Text;
             using (SqlConnection conn = new SqlConnection(new dataBase().path))
             {
                 conn.Open();
                 SqlCommand command = new SqlCommand();
                 command.Connection = conn;
-                command.CommandText = "DELETE FROM ordernotebook WHERE orderid = " + selectedOredrId;
+                command.CommandText = "DELETE FROM order WHERE orderid = " + orderid;
                 command.ExecuteNonQuery();
 
                 btnAll_Click(null, null);
@@ -177,19 +192,17 @@ namespace CSP
         {
             if(type == 1)
             {
-                selectedOredrId = int.Parse(dataGridView1.Rows[e.RowIndex].Cells["orderid"].Value.ToString());
-                inputString.Text = dataGridView1.Rows[e.RowIndex].Cells["stuid"].Value.ToString() +"-"+ dataGridView1.Rows[e.RowIndex].Cells["notebookid"].Value.ToString();
+                textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells["orderid"].Value.ToString();
             }
             else if (type == 2)
             {
-         
                 textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells["stuid"].Value.ToString();
             }
             else
             {
                 textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells["notebookid"].Value.ToString();
             }
-           
+            inputString.Text = dataGridView1.Rows[e.RowIndex].Cells["orderid"].Value.ToString();
         }
 
         private void stuList_Click(object sender, EventArgs e)
@@ -230,10 +243,10 @@ namespace CSP
                     SqlCommand command = new SqlCommand();
 
                     command.Connection = conn;
-                    command.CommandText = "INSERT INTO student(stuid, stuname) VALUES('" + stuid + "','" + name + "');";
+                    command.CommandText = "INSERT INTO student(stuid, name) VALUES('" + stuid + "','" + name + "');";
                     command.ExecuteNonQuery();
 
-                    stuList_Click(null, null);
+                    notList_Click(null, null);
                 }
             }
             catch (Exception ex)
@@ -250,16 +263,23 @@ namespace CSP
                 MessageBox.Show("데이터 테이블에서 아이템을 선택하세요.");
                 return;
             }
-            using (SqlConnection conn = new SqlConnection(new dataBase().path))
+            try
             {
-                conn.Open();
-                SqlCommand command = new SqlCommand();
-                command.Connection = conn;
-                command.CommandText = "DELETE FROM student WHERE stuid = " + stuid;
-                command.ExecuteNonQuery();
+                using (SqlConnection conn = new SqlConnection(new dataBase().path))
+                {
+                    conn.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = conn;
+                    command.CommandText = "DELETE FROM student WHERE stuid = " + stuid;
+                    command.ExecuteNonQuery();
 
-                stuList_Click(null, null);
+                    stuList_Click(null, null);
+                }
+            } catch
+            {
+                MessageBox.Show("다른 DB에서 기본키로 사용된 KEY는 제거가 불가능합니다.");
             }
+            
         }
 
         private void notList_Click(object sender, EventArgs e)
@@ -318,52 +338,23 @@ namespace CSP
                 MessageBox.Show("데이터 테이블에서 아이템을 선택하세요.");
                 return;
             }
-            
-            using (SqlConnection conn = new SqlConnection(new dataBase().path))
+            try
             {
-                conn.Open();
-                SqlCommand command = new SqlCommand();
-                command.Connection = conn;
-                command.CommandText = "DELETE FROM notebook WHERE notebookid = " + noteid;
-                command.ExecuteNonQuery();
+                using (SqlConnection conn = new SqlConnection(new dataBase().path))
+                {
+                    conn.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = conn;
+                    command.CommandText = "DELETE FROM order WHERE notebookid = " + noteid;
+                    command.ExecuteNonQuery();
 
-                notList_Click(null, null);
+                    notList_Click(null, null);
+                }
+            } catch
+            {
+                MessageBox.Show("다른 DB에서 기본키로 사용된 KEY는 제거가 불가능합니다.");
             }
+            
         }
-
-        private void inputName_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        public int GetWeekNumber(int year, int month, int day, DayOfWeek dayOfWeek)
-        {
-            DateTime calculationDate = new DateTime(year, month, day); //주차를 구할 일자 
-            DateTime standardDate = new DateTime(year, 1, 1);   //기준일 
-            Calendar calendarCalc = CultureInfo.CurrentCulture.Calendar;
-            int weekNumber = calendarCalc.GetWeekOfYear(calculationDate, CalendarWeekRule.FirstDay, dayOfWeek)
-                 - calendarCalc.GetWeekOfYear(standardDate, CalendarWeekRule.FirstDay, dayOfWeek) + 1;
-            return weekNumber;
-        }
-
-        public static int GetWeekOfMonth(DateTime dt)
-        {
-            DateTime now = dt;
-
-            int basisWeekOfDay = (now.Day - 1) % 7;
-            int thisWeek = (int)now.DayOfWeek;
-
-            double val = Math.Ceiling((double)now.Day / 7);
-
-            if (basisWeekOfDay > thisWeek) val++;
-
-            return Convert.ToInt32(val);
-
-        }
-
-
     }
-    
 }
-
-
